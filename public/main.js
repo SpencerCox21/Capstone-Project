2459e100385f156f10cf5e20acc927ec83c77539
Spencer Cox <Spencercoxinbox@gmail.com>
2023-10-25 15:22:27 -0600

    Created all four databases

--- public/main.js
+++ public/main.js
@@ -1,56 +1,129 @@
-var cartCountText = document.querySelector("#cart-count");
-var signUpForm = document.querySelector(".email-sign-up");
-var emailInput = document.querySelector("input");
-var signUpBtn = document.querySelector("#sign-up");
-var footer = document.querySelector("footer");
-var blackBtn = document.querySelector("#black-btn");
-var whiteBtn = document.querySelector("#white-btn");
-var greyBtn = document.querySelector("#grey-btn");
-var redBtn = document.querySelector("#red-btn");
-var blueBtn = document.querySelector("#blue-btn");
-var orangeBtn = document.querySelector("#orange-btn");
-var greenBtn = document.querySelector("#green-btn");
-var yellowBtn = document.querySelector("#yellow-btn");
-var purpleBtn = document.querySelector("#purple-btn");
-var maroonBtn = document.querySelector("#maroon-btn");
-var babyBlueBtn = document.querySelector("#babyBlue-btn");
-var pinkBtn = document.querySelector("#pink-btn");
+const baseURL = `http://localhost:5500/public`
 
-// console.log(footer);
+const cartContainer = document.querySelector('#cart-container')
 
-var count = 0;
 
-function addToCart() {
-  count = count + 1;
-  if (count === 1) {
-    cartCountText.textContent = count + " Item";
-  } else {
-    cartCountText.textContent = count + " Items";
-  }
-}
 
-function emailSubmit() {
-  var confirmationMessage = document.createElement("p");
-  confirmationMessage.textContent =
-    "Thank you for signing up to our newsletter, we sent a confirmation email to " +
-    emailInput.value;
 
-  signUpForm.remove();
 
-  footer.appendChild(confirmationMessage);
-}
 
-signUpBtn.addEventListener("click", emailSubmit);
 
-blackBtn.addEventListener("click", addToCart);
-whiteBtn.addEventListener("click", addToCart);
-greyBtn.addEventListener("click", addToCart);
-redBtn.addEventListener("click", addToCart);
-blueBtn.addEventListener("click", addToCart);
-orangeBtn.addEventListener("click", addToCart);
-greenBtn.addEventListener("click", addToCart);
-yellowBtn.addEventListener("click", addToCart);
-purpleBtn.addEventListener("click", addToCart);
-maroonBtn.addEventListener("click", addToCart);
-babyBlueBtn.addEventListener("click", addToCart);
-pinkBtn.addEventListener("click", addToCart);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cartCountText = document.querySelector("#cart-count");
+// var signUpForm = document.querySelector(".email-sign-up");
+// var emailInput = document.querySelector("input");
+// var signUpBtn = document.querySelector("#sign-up");
+// var footer = document.querySelector("footer");
+// var blackBtn = document.querySelector("#black-btn");
+// var whiteBtn = document.querySelector("#white-btn");
+// var greyBtn = document.querySelector("#grey-btn");
+// var redBtn = document.querySelector("#red-btn");
+// var blueBtn = document.querySelector("#blue-btn");
+// var orangeBtn = document.querySelector("#orange-btn");
+// var greenBtn = document.querySelector("#green-btn");
+// var yellowBtn = document.querySelector("#yellow-btn");
+// var purpleBtn = document.querySelector("#purple-btn");
+// var maroonBtn = document.querySelector("#maroon-btn");
+// var babyBlueBtn = document.querySelector("#babyBlue-btn");
+// var pinkBtn = document.querySelector("#pink-btn");
+
+
+// var count = 0;
+
+// function addToCart() {
+//   count = count + 1;
+//   if (count === 1) {
+//     cartCountText.textContent = count + " Item";
+//   } else {
+//     cartCountText.textContent = count + " Items";
+//   }
+// }
+
+// function emailSubmit() {
+//   var confirmationMessage = document.createElement("p");
+//   confirmationMessage.textContent =
+//     "Thank you for signing up to our newsletter, we sent a confirmation email to " +
+//     emailInput.value;
+
+//   signUpForm.remove();
+
+//   footer.appendChild(confirmationMessage);
+// }
+
+// signUpBtn.addEventListener("click", emailSubmit);
+
+// blackBtn.addEventListener("click", addToCart);
+// whiteBtn.addEventListener("click", addToCart);
+// greyBtn.addEventListener("click", addToCart);
+// redBtn.addEventListener("click", addToCart);
+// blueBtn.addEventListener("click", addToCart);
+// orangeBtn.addEventListener("click", addToCart);
+// greenBtn.addEventListener("click", addToCart);
+// yellowBtn.addEventListener("click", addToCart);
+// purpleBtn.addEventListener("click", addToCart);
+// maroonBtn.addEventListener("click", addToCart);
+// babyBlueBtn.addEventListener("click", addToCart);
+// pinkBtn.addEventListener("click", addToCart);
